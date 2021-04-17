cf6d34af12c31fbe938b1de3be7a3708bd4d11ba
reymyr <reyhanemyr51@gmail.com>
2021-04-17 22:42:31 +0700

    ui sementara done

--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+messages=[]
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', messages=messages)
+
+@app.route('/send', methods=['POST'])
+def send():
+    msg = request.form['message']
+    if msg:
+        messages.append(['sent', request.form['message']])
+        messages.append(['received', request.form['message'][::-1]])
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True,threaded=True)
