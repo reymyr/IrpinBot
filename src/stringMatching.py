1e577e7e220c21b2d52c786992c4972596fdc1e1
reymyr <reyhanemyr51@gmail.com>
2021-04-27 14:52:32 +0700

    fix circular import

--- src/stringMatching.py
+++ src/stringMatching.py
@@ -1,6 +1,6 @@
-from app import keywords
 import re
 
+keywords = ["Kuis", "Ujian", "Tucil", "Tubes", "Praktikum"]
 months = {'januari': '1', 'februari':'2', 'maret':'3', 'april':'4','mei':'5','juni':'6','juli':'7','agustus':'8', 'september':'9',
             'oktober':'10','november':'11','desember':'12'}
 
